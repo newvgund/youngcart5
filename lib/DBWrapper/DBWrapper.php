1fc875c558251ef38a4ad2690fb97851305fe764
kri <9606>
2014-10-15 02:20:49 +0900

    php 클래스 파일 재 정의
    * php 는 클래스 하나가 하나의 파일로 정의하지 않으며, 성능 문제로 권장하지도 않는 듯?

--- lib/DBWrapper/DBWrapper.php
+++ lib/DBWrapper/DBWrapper.php
@@ -1,9 +1,149 @@
 <?php
-require_once('MySqlConnection.php');
+// Base Classes
+abstract class Connection implements ConnectionController
+{
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+    private $port;
 
+    /**
+     * @return mixed
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * @param mixed $port
+     */
+    public function setPort($port)
+    {
+        $this->port = $port;
+    }
+
+    private $database_prefix;
+
+    /**
+     * @return mixed
+     */
+    public function getDatabase()
+    {
+        return $this->database;
+    }
+
+    /**
+     * @param mixed $database
+     */
+    public function setDatabase($database)
+    {
+        $this->database = $database;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDatabasePrefix()
+    {
+        return $this->database_prefix;
+    }
+
+    /**
+     * @param mixed $database_prefix
+     */
+    public function setDatabasePrefix($database_prefix)
+    {
+        $this->database_prefix = $database_prefix;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param mixed $host
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+}
+
+
+// interface
+interface ConnectionController
+{
+    public function connect();
+    public function disconnect();
+}
+
+
+// Connection Classes
+class MySqlConnection extends Connection
+{
+    function __construct($hostname = NULL, $username = NULL, $password = NULL, $database = NULL, $port = NULL)
+    {
+        $this->setHost($hostname);
+        $this->setUsername($username);
+        $this->setPassword($password);
+        $this->setDatabase($database);
+        $this->setPort($port);
+    }
+
+    public function connect()
+    {
+        // TODO: Implement connect() method.
+    }
+
+    public function disconnect()
+    {
+        // TODO: Implement disconnect() method.
+    }
+}
+
+
+// DBWrapper
 class DBWrapper
 {
-    private $connection  // connection
+    private $connection;    // connection
 
     public function __construct()
     {
@@ -22,4 +162,3 @@ class DBWrapper
         return $instance;
     }
 }
-?>
